9eca13c999114c2719fa7db2da8adaed0116486c
rudy <rudolf.zachar@gmail.com>
2020-07-27 21:53:50 +0100

    New Vue instance / el,  data(array of to-do's), methods (addNewItem) created + corresponding HTML elements created

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    new Vue({
+        el: "#app",
+        data: {
+            todos: ['Buy shopping', 'Tidy up garage', 'Wash the car','Look up holiday'],
+            newItem: ''
+        },
+        methods: {
+            addNewItem: function(){
+                this.todos.push(this.newItem);
+                this.newItem = '';
+            }
+            
+        }
+    }); 
 
-
-    
 });
