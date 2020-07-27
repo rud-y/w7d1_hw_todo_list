a673a8065cf10e016285902eef112a6faaf7cd05
rudy <rudolf.zachar@gmail.com>
2020-07-28 00:47:41 +0100

    setHigh method created

--- src/app.js
+++ src/app.js
@@ -5,13 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            todos: ['Buy shopping', 'Tidy up garage', 'Wash the car','Look up holiday'],
+            todos: [
+                {name: 'Buy groceries', priority: true},
+                {name: 'Tidy up garage', priority: false},
+                {name: 'Look up holiday', priority: true}
+            ],
             newItem: ''
         },
         methods: {
             addNewItem: function(){
-                this.todos.push(this.newItem);
+                this.todos.push({
+                    name: this.newItem,
+                    priority: false
+                });
+
                 this.newItem = '';
+            },
+
+            setHigh: function(index){
+                this.todos[index].priority = true;
             }
             
         }
